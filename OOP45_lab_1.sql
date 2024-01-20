b8eff72dd1ccb5853372dca3e262f9a18c545e93
IrynaBarnych <iryna.barnych@gmail.com>
2024-01-20 19:57:11 +0200

    Виведіть назви відділень, в яких лікарі не отримують надбавки.

--- OOP45_lab_1.sql
+++ OOP45_lab_1.sql
@@ -1,11 +1,10 @@
--- Виведіть назви відділень, які отримували пожертвування
--- у розмірі понад 100000, із зазначенням їх лікарів.
-
+-- Виведіть назви відділень, в яких лікарі не отримують надбавки.
 
 SELECT DISTINCT Departments.Name AS "Назва відділення"
-FROM Departments 
-JOIN Donations ON Departments.Id = Donations.Department_Id
-WHERE Donations.amount > 10000;
-
-
-
+FROM Departments
+WHERE NOT EXISTS (
+    SELECT 1
+    FROM DoctorsSpecializations
+    JOIN Doctors ON DoctorsSpecializations.doctor_id = Doctors.Id
+    WHERE DoctorsSpecializations.specialization_id = Departments.Id AND Doctors.Salary = 0
+);
