e9f0e9ddeca3112920374965f413e12e0fe75b9b
IrynaBarnych <iryna.barnych@gmail.com>
2024-01-20 20:24:39 +0200

    Виведіть назви відділень і назви захворювань, обстеження з яких вони проводили за останні півроку.

--- OOP45_lab_1.sql
+++ OOP45_lab_1.sql
@@ -1,10 +1,28 @@
--- Виведіть назви відділень, в яких лікарі не отримують надбавки.
+-- Виведіть назви відділень і назви захворювань, 
+-- обстеження з яких вони проводили за останні півроку.
 
-SELECT DISTINCT Departments.Name AS "Назва відділення"
-FROM Departments
-WHERE NOT EXISTS (
-    SELECT 1
-    FROM DoctorsSpecializations
-    JOIN Doctors ON DoctorsSpecializations.doctor_id = Doctors.Id
-    WHERE DoctorsSpecializations.specialization_id = Departments.Id AND Doctors.Salary = 0
-);
+SELECT DISTINCT
+    Departments.Name AS "Назва відділення",
+    Diseases.Name AS "Назва захворювання"
+FROM
+    Departments
+JOIN
+    Examinations ON Departments.Id = Examinations.Id
+JOIN
+    Diseases ON Examinations.Id = Diseases.Id
+	
+WHERE 
+	EXTRACT(MONTH FROM '2024-01-01'::DATE) BETWEEN 1 AND 6;
+
+
+
+
+
+
+
+
+
+
+
+
+
